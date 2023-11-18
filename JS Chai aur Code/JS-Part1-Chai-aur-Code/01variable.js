8324850c752b99fd7b61c8665b52ead55b16b4bb
Aamir Hussain Azad <112759050+Aamiruem@users.noreply.github.com>
2023-11-18 16:24:08 +0530

    Add files via upload

---
+++ JS Chai aur Code/JS-Part1-Chai-aur-Code/01variable.js
@@ -0,0 +1,33 @@
+// accountId = 13547689
+// accountEmail = "aamir786@gmail.com"
+// accountPassword = "12345"
+// accountCity = "Narkatiaganj"
+
+
+// //accountId = 2
+// console.log(accountId)
+// console.table(accountId, accountEmail, accountPassword, accountCity)
+
+
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
